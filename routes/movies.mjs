927f192ad645e47ca87a9b6ab4223745715e2748
AndresBarraganM <al22760549@ite.edu.mx>
2025-01-18 18:26:14 -0800

    controlador agregado

--- routes/movies.mjs
+++ routes/movies.mjs
@@ -1,76 +1,15 @@
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from '../schemas/movies.mjs'
-import { MovieModel } from '../models/movie.mjs'
-
-/*
-Formas de importar un json
-
-import movies from './movies.json' with { type: 'json' } 
-  experimentado por lo tanto no oficial y la sintaxis puede cambiar
-
-
-import fs from 'node:fs'
-const movies = JSON.parse(fs.readFileSync('./movies.json','utf-8'))
-  otra forma de importar un json pero requiere importar fs
-*/
-//forma recomendada de importar un json por ahora
-import { createRequire } from 'node:module'
-const require = createRequire(import.meta.url)
-const movies = require('../movies.json')
-
+import { MovieController } from '../controllers/movies'
 
 export const MoviesRouter = Router()
 
-MoviesRouter.get('/', async (req, res) => {
-  const { genre } = req.query
+MoviesRouter.get('/', MovieController.getAll)
 
-  const movies = await MovieModel.getAll({ genre })
+MoviesRouter.get('/:id', MovieController.getById)
 
-  res.json(movies)
-})
+MoviesRouter.post('/', MovieController.create)
 
-MoviesRouter.get('/:id', async (req,res) => {
-  const { id } = req.params
-  const movie = await MovieModel.getById({ id })
-  if (movie) return res.json(movie)
-  res.status(404).json({message: 'Movie not found'})
-})
+MoviesRouter.delete('/:id', MovieController.delete)
 
-MoviesRouter.post('/', async (req, res) => {
-    const result = validateMovie(req.body)
-  
-    if (!result.success) {
-      // 422 Unprocessable Entity
-      return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-
-    const newMovie = await MovieModel.create(result.data)
-  
-    res.status(201).json(newMovie)
-})
-
-MoviesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-
-  const result = await MovieModel.delete({ id })
-
-  if (result == false) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  return res.json({ message: 'Movie deleted' })
-})
-
-MoviesRouter.patch('/:id', async(req, res) => {
-  const result = validatePartialMovie(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const updateMovie = await MovieModel.update({ id, input: result.data})
-
-  return res.json(updateMovie)
-})
+MoviesRouter.patch('/:id', MovieController.update)
 
